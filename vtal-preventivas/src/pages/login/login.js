22b6ecf258aa2f340471fe22f6247eff92636bf5
euerika <euerika.soaresdossantos@gmail.com>
2023-07-20 12:00:23 -0300

    atualização

--- vtal-preventivas/src/pages/login/login.js
+++ vtal-preventivas/src/pages/login/login.js
@@ -47,29 +47,4 @@ export const Login = () => {
 
 export default Login;
 
-// function NomeInput () {
-//     const [nomeFuncionario, setNomeFuncionario] = useState('');
-//     const navigate = useNavigate();
 
-//     const handleSubmit = (event) => {
-//     event.preventDefault();
-//     navigate(`/atendimento/${nomeFuncionario}`);
-//     }
-
-//     return (
-//         <div>
-//           <form onSubmit={handleSubmit}>
-//             <label>
-//               Nome do Funcionário:
-//               <input
-//                 type="text"
-//                 value={nomeFuncionario}
-//                 onChange={(e) => setNomeFuncionario(e.target.value)}
-//               />
-//             </label>
-//             <button type="submit">Enviar</button>
-//           </form>
-//         </div>
-//     );
-// }
-// export default NomeInput;
